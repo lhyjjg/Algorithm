888615e9dd90140ce74ab5962bfebb5b669a6a66
hyeonyoungLee <120166543+lhyjjg@users.noreply.github.com>
2023-12-09 21:17:07 +0900

    [level unrated] Title: 카운트 업, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,7 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    for (start_num; start_num <= end_num; start_num++) {
+        answer.push(start_num);
+    }
+    return answer;
+}
